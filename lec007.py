b2854f7d80cd57b5c7fbd8bd37053cc76cab8266
MaxX <33282787+m1644@users.noreply.github.com>
2022-09-22 10:12:17 +0300

    Add lec007

---
+++ lec007.py
@@ -0,0 +1,19 @@
+# Управляющие конструкции:
+# for
+
+for i in 1,2,3,4,5:
+    print(i**2)
+
+list = [1,2,3,4,10,5]  
+for i in list:
+    print(i) 
+
+r = range(10)
+for i in r: 
+    print(i)
+
+for i in range(5):
+    print(i)
+
+for i in range(1, 10, 2):
+    print(i)
