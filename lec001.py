a9fe5bd871d9c99d37cffa2bed17e1e1a7f12715
MaxX <33282787+m1644@users.noreply.github.com>
2022-09-22 10:10:55 +0300

    Add lec001

---
+++ lec001.py
@@ -0,0 +1,25 @@
+# Типы данных и переменная
+# int, float, boolean, str, list, None
+
+value = None
+print(type(value))
+a = 123
+b = 1.23
+print(type(a))
+print(type(b))
+value = 12334
+print(type(value))
+
+s = 'hello \nworld'
+print(s) # вывод строки
+print(a, '-', b, '-', s)
+print('{} - {} - {}'.format(a, b, s))
+print(f'{a} - {b} - {s}')
+
+f = True
+print(f)
+
+list = [1, 2, 3]
+col = ['hello', 1,2,4.5,True]
+print(list)
+print(col)
