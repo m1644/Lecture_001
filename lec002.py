ef83bf5cc61e78564766035ac954b813eff5f513
MaxX <33282787+m1644@users.noreply.github.com>
2022-09-22 10:11:19 +0300

    Add lec002

---
+++ lec002.py
@@ -0,0 +1,11 @@
+# Ввод и вывод данных
+# print(), input()
+
+print('Введите a');
+a = int(input())
+print('Введите b');
+b = float(input())
+print(a, b)
+print('{} {}'.format(a, b))
+print(f'{a} {b}')
+print(a, '+' , b, '=', a+b)
